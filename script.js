88c3ede394f4195c53e2c9fd714d69952dd828b8
Esteban Mendez <edmendezn@gmail.com>
2021-01-04 11:57:58 -0500

    Added array of questions and answers

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ var welcomeIntro = document.getElementById('welcome');
 var container_boxElement = document.getElementById('container-box');
 var nextButton = document.getElementById('next-btn');
 var questionElement = document.getElementById('questions');
-var optionElement = document.getElementById('option');
+var optionElement = document.querySelectorAll('#option');
 
 startButton.addEventListener('click', startQuiz);
 
 var arrOfQuestions = [
     {
         question:"What is 2 plus 2?",
-        answer:['2','4','67','5'],
-        correct:'4'
     },
     {
         question:"What is 30 into 10?",
-        answer:['3','4','7','53'],
-        correct:'3'
+       
     },
 ]
+
+var arrOfAnswers = ['2','3','4','5'];
 //...this function will execute when I click on the Start button...
 function startQuiz(){
-    welcomeIntro.classList.add('hide');
     startButton.classList.add('hide');
+    welcomeIntro.classList.add('hide');
     container_boxElement.classList.remove('hide');
     nextButton.classList.remove('hide');
     nextQuestion()
@@ -33,7 +32,11 @@ function startQuiz(){
 //...this function will execute when clickin on the next button...
 function nextQuestion(){
     questionElement.innerText = arrOfQuestions[0].question
-    optionElement.innerText = '';
+    for (var i = 0; i < arrOfAnswers.length; i++){
+        console.log(optionElement[i]);
+        console.log(arrOfAnswers[i]);
+        optionElement[i].innerText = arrOfAnswers[i];
+    }
     
 }   
 //...this will execute when I choose an answer...
