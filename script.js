aa56cc67de458150380c85c306648255ff829086
Esteban Mendez <edmendezn@gmail.com>
2021-01-14 09:32:25 -0500

    Add local storage

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// Grabbing elements from html page
-var startButton = document.getElementsById("start-btn");
+// Grab elements from html page
+var startButton = document.getElementById("start-btn");
 var timeEl = document.querySelector(".timer");
 var correctAnswer = document.querySelector(".correctAnswer");
 var wrongAnswer = document.querySelector(".wrongAnswer");
 var container = document.querySelector(".container");
 
-// Render last user container code
+// Grab elements for renderring last user (localStorage)
 var lastUserContainer = document.querySelector("#lastUser-Container");
 var userInfo = document.querySelector("#userInfo");
 var initials = document.querySelector(".initials");
 var score = document.querySelector(".score");
 var lastUser = document.querySelector("#lastUser");
-var saveDiv = document.querySelectord(".save-div");
+var saveDiv = document.querySelector(".save-div");
 var saveBtn = document.querySelector("#save");
 var lastRegistered = document.querySelector(".lastRegistered");
 
@@ -20,8 +20,65 @@ var seconds = 60;
 var questionIndex = 0;
 var correct = 0;
 var wrong = 0;
-
-startButton.addEventListener("click", startQuiz);
+var quizContainer = document.querySelector("#quizContainer");
+var myQuestions = [
+  {
+    question: "What does CSS stand for?",
+    answerIndex: 2,
+    choices: [
+      "Clerical Support Staff",
+      "Charles Smith Sonye",
+      "Cascading Styling Sheets",
+      "College Scholarship Service",
+    ],
+  },
+  {
+    question: "JavaScript File Has An Extension Of?",
+    answerIndex: 1,
+    choices: [".java", ".js", ".script", ".JAVASCRIPT"],
+  },
+  {
+    question: "Difference between “==” and “===” ?",
+    answerIndex: 3,
+    choices: [
+      "There is no difference",
+      "Both only compares values",
+      "Both compare values and types",
+      "== only compares values and === compares values and type",
+    ],
+  },
+  {
+    question: "Select the correct opening and closing tags",
+    answerIndex: 2,
+    choices: [
+      "<h1><h1>",
+      "<heading></heading>",
+      "<button></button>",
+      "-button- -button-",
+    ],
+  },
+  {
+    question: "How to add one new element at end of an array in javascript?",
+    answerIndex: 0,
+    choices: [
+      "Use the push() method",
+      "Use the add() method",
+      "Concatenate the new element to the array",
+      "Create a new array that contains the new element",
+    ],
+  },
+  {
+    question:
+      "What is the correct JavaScript syntax to change the content of the following HTML element: <p id='demo'>change this line.</p>",
+    answerIndex: 1,
+    choices: [
+      "#demo.innerHTML = 'Hello World'",
+      "document.getElementById('demo').innerHTML = 'Love coding'",
+      "document.querySelector('demo').innerHTML = 'I like cats'",
+      "There is no correct way to do this because life is multifaceted",
+    ],
+  },
+];
 
 // WHEN I click the Start button, the startQuiz function will execute,
 // The Start button will hide.
@@ -39,65 +96,40 @@ function startQuiz() {
 // When the seconds equals to zero the timer will stop.
 // The timer, question, and four choices will hide.
 // The renderLastUser function will execute.
-var seconds = 60;
 function setTime() {
   var timerInterval = setInterval(function () {
     seconds--;
     timeEl.textContent = seconds + " seconds left";
     if (seconds === -1) {
       clearInterval(timerInterval);
-      container.classList.add("hide");
-      lastUserContainer.classList.remove("hide");
-      renderLastUser();
+      highScores();
     }
   }, 1000);
 }
 
-// The questions and choices will be grabbed from the array of questions
-var myQuestions = [
-  {
-    question: "What does CSS stand for?",
-    answerIndex: 2,
-    choices: [
-      "Clerical Support Staff",
-      "Charles Smith Sonye",
-      "Cascading Styling Sheets",
-      "College Scholarship Service",
-    ],
-  },
-  {
-    question: "JavaScript File Has An Extension Of?",
-    answerIndex: 1,
-    choices: [".java", ".js", ".script", ".JAVASCRIPT"],
-  },
-];
-
-// The containerTag will target an html element with an id of 'container'.
-var quizContainer = document.querySelector("#quizContainer");
-
-function showQuestion() {
+function showNextQuestion() {
   quizContainer.textContent = "";
 
   // Create an h1 element that is a child of the containerTag
   // and will hold the questions from the array of questions.
   var questionTag = document.createElement("h1");
   quizContainer.appendChild(questionTag);
-  questionTag.textContent = myQuestions[questionIndex].question;
-  
+  questionTag.textContent = myQuestions[questionIndex].question; // this displays the question on page
+
   // Create a ul element that is a child of quizContainer
   var choiceListTag = document.createElement("ul");
   quizContainer.appendChild(choiceListTag);
 
   // Create a loop and li element to hold each of the choices,
   // the li element is a child of choiceListTag.
-  for (var i = 0; i < myQuestions[questionIndex].choices.length; i++) {
+  for (var i = 0; i < myQuestions[questionIndex].choices.length; i++) { // loop for choices only
     var choiceTag = document.createElement("li");
     choiceListTag.appendChild(choiceTag);
 
     var button = document.createElement("button");
     choiceTag.appendChild(button);
     choiceTag.style.width = "fit-content";
-    button.textContent = myQuestions[questionIndex].choices[i]
+    button.textContent = myQuestions[questionIndex].choices[i]; // this displays the choices
 
     button.setAttribute("data-index", i);
     var index = parseInt(button.getAttribute("data-index"));
@@ -106,22 +138,22 @@ function showQuestion() {
     // then the browser will display "Correct" if the correct choice is clicked on
     // Or "Wrong" if any of the other choices are clicked on.
     // Add 1 to the count of correct or wrong variables.
-    if(index === myQuestions[questionIndex].answerIndex){
-      choiceListTag.addEventListener("click", function(event){
+    if (index === myQuestions[questionIndex].answerIndex) {
+      choiceListTag.addEventListener("click", function (event) {
         alert("Wrong Choice");
         event.stopPropagation();
-        wrong++
+        wrong++;
         wrongAnswer.textContent = "Wrong answers = " + wrong;
-        questionIndex++
+        questionIndex++;
         showNextQuestion();
       });
 
-      button.addEventListener("click", function(event){
+      button.addEventListener("click", function (event) {
         alert("Correct Choice");
         event.stopPropagation();
-        correct++
+        correct++;
         correctAnswer.textContent = "Correct answers = " + correct;
-        questionIndex++
+        questionIndex++;
         showNextQuestion();
       });
     }
@@ -130,20 +162,59 @@ function showQuestion() {
 
 // WHEN the seconds is equal to zero, the renderLastUser function will execute.
 // The user will enter their initials and click Save.
-function displayMessage(type, message){
+
+function displayMessage(type, message) {
   saveDiv.textContent = message;
   saveDiv.setAttribute("class", type);
 }
 
-function renderLastUser(){
+function renderLastUser() {
   var userInitials = localStorage.getItem("userInitials");
   //need to do one for the score
 
-  if(!userInitials){
+  if (!userInitials) {
     return "";
   }
 
   var lastUserEl = document.createElement("h2");
   lastRegistered.appendChild(lastUserEl);
-  
+  lastRegistered.textContent = "Last User Registered";
+
+  var lastUser = document.createElement("h3");
+  lastRegistered.appendChild(lastUser);
+  lastUser.textContent = userInitials;
+}
+
+function highScores() {
+  container.classList.add("hide");
+  lastUserContainer.classList.remove("hide");
+  renderLastUser();
+  document.getElementById("yourScore").textContent =
+    "Your Score is " + correct + "/6";
+  //document.getElementById("yourScore").style.color = "white";
+}
+
+saveBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  // console.log(event.target)
+  var userInitials = document.querySelector(".initials").value;
+  //var userScore =
+
+  if (userInitials === "") {
+    displayMessage("Error", "Please, enter your initials");
+  } else {
+    displayMessage("Success", "Initials saved!");
+  }
+
+  var userScore = userInitials + " " + correct + "/6"
+  var storage = JSON.parse(localStorage.getItem("highScores"));
+  storage.push(userScore)
+  localStorage.setItem("highScores", JSON.stringify(storage));
+  renderLastUser();
+});
+
+startButton.addEventListener("click", startQuiz);
+
+if (localStorage.getItem("highScores") === null) {
+  localStorage.setItem("highScores", JSON.stringify([]));
 }
